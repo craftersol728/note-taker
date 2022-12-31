333dd860fc35d95c7ce6e713e49d7b74a9be96f3
Jerry Wan <craftersol728@gmail.com>
2022-12-31 13:16:48 -0800

    connected routes together using app.use

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
-const express = require ('express')
-const app = express ()
+const express = require ('express');
+const app = express ();
+
+const fs = require ('fs');
+const routes = require('routes');
+
+//require routes code from routes folder
+const apiRoutes = require ('./routes/apiRoutes');
+const htmlRoutes = require ('./routes/htmlRoutes');
+
+// It parses incoming requests with urlencoded payloads and is based on body-parser.
+app.use(express.urlencoded({
+    extended: true
+}));
+
+//makes clear what the root directory is
+app.use(express.static('public'));
+app.use(express.json());
+
+
+//connecting routes together
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
