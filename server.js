435262f179310171cff94e68f1bfa8339acc2959
Jerry Wan <craftersol728@gmail.com>
2023-01-06 21:13:50 -0800

    added delete note function now need to deploy to heroku

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3069;
 //creates server on port 3001
 const fs = require('fs');
 const path = require('path');
@@ -30,6 +30,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+
+//created new note function that addes a new note and stores the array into an object
 function addNewNote(body, notesArray) {
     const newNote = body;
     if (!Array.isArray(notesArray))
@@ -40,12 +42,43 @@ function addNewNote(body, notesArray) {
 
     body.id = notesArray[0];
     notesArray[0]++;
-
+//write file sync https://www.geeksforgeeks.org/node-js-fs-writefilesync-method/
     notesArray.push(newNote);
     fs.writeFileSync(
+        //The path.join() method joins the specified path segments into one path.
         path.join(__dirname, './db/db.json'),
         JSON.stringify(notesArray, null, 2)
     );
+    //returns the final and updated note
     return newNote;
 }
 
+app.post('/api/notes', (req, res) => {
+    const newNote = addNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+//delete note function
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+//the delete note function is called 
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+//console logs the port  it is on to the terminal in this case 3069
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
